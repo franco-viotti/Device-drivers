d67cf20330b87dbc82331ffa12874602e839ea7c
franco-viotti <franviotti@gmail.com>
2023-06-20 20:33:47 +0000

    Se modifica de donde lee el py

--- read_filedev.py
+++ read_filedev.py
@@ -1,7 +1,7 @@
 import os
 
 # Abrir el archivo de dispositivo en modo de lectura y escritura
-dev = os.open('/dev/ttyUSB0', os.O_RDWR)
+dev = os.open('/dev/mychardev', os.O_RDWR)
 
 # Leer los datos del archivo de dispositivo
 data = os.read(dev, 1024)
