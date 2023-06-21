274c4e0a540527ae83edf991df6f68d032e11ab6
franco-viotti <franviotti@gmail.com>
2023-06-21 19:05:36 +0000

    Se cambia el nombre de la app

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from flask import Flask, render_template
 import os
 
-botones = Flask(__name__)
+app = Flask(__name__)
 
-@botones.route('/')
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@botones.route('/sensar_boton')
+@app.route('/sensar_boton')
 def sensar_boton():
     os.system('echo 0 > /dev/my_gpio_driver')
     return 'Señal del botón sensada.'
 
-@botones.route('/sensar_microfono')
+@app.route('/sensar_microfono')
 def sensar_microfono():
     os.system('echo 1 > /dev/my_gpio_driver')
     return 'Señal del micrófono sensada.'
 
 if __name__ == '__main__':
-    botones.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
